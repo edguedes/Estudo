92f66d8f53c64c7ff5c408d01e68915656a4693d
Walter <edsouzaguedes@gmail.com>
2023-11-19 16:41:02 -0300

    Revisando vetor

---
+++ Estrutura_Dados/src/main/java/org/example/vetor/Vetor_01.java
@@ -0,0 +1,10 @@
+package org.example.vetor;
+
+public class Vetor_01 {
+
+    private String[] elements;
+
+    public Vetor_01(int capacity) {
+        this.elements = new String[capacity];
+    }
+}
