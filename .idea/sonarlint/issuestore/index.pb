1cd01c7980a6f593902341275a2997da15753e43
Walter <edsouzaguedes@gmail.com>
2024-06-06 12:31:37 -0300

    Estudo

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,27 @@
+
+T
+$Collections/src/Recursos_Collections,1/7/17c595045b4102ec90a2e9ecf17b0052a2929fbc
+I
+Collections/src/Main.java,4/f/4f89c8751d9c90eb63a4deaa6245d29403f90439
+M
+Collections/src/Lista_02.java,7/3/733be508a83f47f2df2103de6b795c3dae0c9fbd
+X
+(Collections/src/model/AnalisandoSet.java,b/5/b5d77fd2f4d9f74aa0014f938800b32a3678f3dd
+O
+Collections/src/model/Aula.java,7/c/7c47781e9d4bd389d291a5b4f85a96a2b5da97c1
+W
+'Collections/src/TestaCursoComAluno.java,6/b/6b2af5e3ed7afcf79064fc172a581b3cd3b190f5
+P
+ Collections/src/ExemploList.java,2/2/2286ca13dfeb606fa65d633450a4322de7754251
+U
+%Collections/src/PerformaceListas.java,c/9/c94b8456e7143a00556024e5447db9b644845e6e
+P
+ Collections/src/model/Curso.java,c/9/c9a27ec1d574df10a9764b6668875432055f2db3
+Y
+)Collections/src/ExemploLinkedHashSet.java,3/a/3a1feabda4a8d7a1d20c4a947e38226b3c2203a7
+P
+ Collections/src/model/Aluno.java,7/2/72ee66b90222a7c25ae7fa35a46dcdf3c865137f
+V
+&Collections/src/ExemploLinkedList.java,0/2/02ae11c7934062c182791ce27029ef8a50cdd37e
+Q
+!Collections/src/ListasBasico.java,7/f/7f19ef55118ba0d95f1a116fdb92ca114792e95a
--- Collections/src/ListasBasico.java
+++ Collections/src/ListasBasico.java
@@ -5,26 +5,39 @@ public class ListasBasico {
 
     public static void main(String[] args) {
 
-        String aula_01 = "Aprofundando em listas";
-        String aula_02 = "Modelando listas";
-        String aula_03 = "Mais de listas";
-        String aula_04 = "Romovendo de uma lista";
+        int numero = 10;
+        if (numero > 0) {
+            System.out.println("O número é positivo");
+        }
 
-        ArrayList<String> listaAulas = new ArrayList<>();
+        if (numero > 0) {
+            System.out.println("O número é positivo");
+        } else if (numero < 0) {
+            System.out.println("O número é negativo");
+        } else {
+            System.out.println("O número é zero");
+        }
 
-        listaAulas.add(aula_01);
-        listaAulas.add(aula_02);
-        listaAulas.add(aula_03);
-        listaAulas.add(aula_04);
-
-        System.out.println(listaAulas);
-        listaAulas.remove(3);
-        System.out.println(listaAulas);
-
-        listaAulas.forEach(aula -> System.out.println(aula));
-
-        //TODO Ordenando uma lista
-        Collections.sort(listaAulas);
+//        String aula_01 = "Aprofundando em listas";
+//        String aula_02 = "Modelando listas";
+//        String aula_03 = "Mais de listas";
+//        String aula_04 = "Romovendo de uma lista";
+//
+//        ArrayList<String> listaAulas = new ArrayList<>();
+//
+//        listaAulas.add(aula_01);
+//        listaAulas.add(aula_02);
+//        listaAulas.add(aula_03);
+//        listaAulas.add(aula_04);
+//
+//        System.out.println(listaAulas);
+//        listaAulas.remove(3);
+//        System.out.println(listaAulas);
+//
+//        listaAulas.forEach(aula -> System.out.println(aula));
+//
+//        //TODO Ordenando uma lista
+//        Collections.sort(listaAulas);
     }
 
 }
